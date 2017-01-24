4a568c4f1ede3d22132f8a6f41a187e02d2d6ba9
BilAbd <bilalabdusattarov@gmail.com>
2017-01-24 16:04:59 +0600

    Lab 1

---
+++ PrimeOrNot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+	public class PrimeOrNot
+	{
+		static void Main(string[] args)
+		{
+			int a = Convert.ToInt16(Console.ReadLine());
+			if (function.bil(a) == true)
+			{
+				Console.Write("Prime");
+			}
+			else
+			{
+				Console.Write("Composed");
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ function.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace lab1
+{
+	public class function
+
+	{
+		public static Boolean bil(int n)
+		{
+			Boolean ok = true;
+			for (int i = 3; i < n; i++)
+			{
+				if (n < 100 && n % i == 0 || n % 2 == 0 || n % 3 == 0)
+					ok = false;
+				else if (n > 100 && n % Convert.ToInt16(Math.Pow(i, 2)) != 0)
+					ok = false;
+				else
+					ok = true;
+			}
+			return ok;
+				
+		}
+
+
+
+	}
+}
